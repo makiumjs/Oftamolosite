8c93e76d3dff2d9cffc6fae7589be7066dc2a7d9
makiumjs <108535575+makiumjs@users.noreply.github.com>
2022-07-03 11:57:19 +0200

    Update services.blade.php

--- resources/views/services.blade.php
+++ resources/views/services.blade.php
@@ -14,7 +14,7 @@
                     <div>
                         <h2>Controllo della Vista</h2>
                         <p>L’occhio è il nostro organo di senso più importante. Per questo una visione ottimale è fondamentale. Quando è stata l’ultima volta che vi siete sottoposti ad un controllo degli occhi?</p>
-                        <button class="detail-button mb-3">Dettagli</button>
+                        <button class="detail-button mb-3" href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjVzdDfudz4AhX_Q_EDHSrqBooQFnoECAoQAQ&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FTest_visivi&usg=AOvVaw30rvV4fJtIAaOngjQee0be">Dettagli</button>
                     </div>
                 </div>
             </div>
@@ -30,7 +30,7 @@
                         <h2>Chirurgia Oftalmica</h2>
                         <p>egli ultimi anni ha visto una grande espansione grazie all’adozione della tecnologia laser, che diminuisce sia i tempi di esecuzione che i tempi di recupero del paziente. La chirurgia oftalmica può risolvere sia patologie come la cataratta, che il glaucoma o il distacco della retina, sia disfunzioni come la miopia e la presbiopia. Oggi gli interventi di chirurgia oftalmica vantano un’ampia percentuale di successi sia per interventi alla cataratta o per glaucoma, che per la correzione di difetti visivi.</p>
                         
-                        <button class="detail-button mb-3">Dettagli</button>
+                        <button class="detail-button mb-3" href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjl1frsudz4AhVrXfEDHY0cBAQQFnoECBIQAQ&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FChirurgia_refrattiva&usg=AOvVaw3gO2Zoamt1ww3Gt2zRGcmk">Dettagli</button>
                     </div>
                 </div>
                 <div class="col-12 col-xl-6 d-flex justify-content-center order-1 order-xl-2">
@@ -54,7 +54,7 @@
                     <div>
                         <h2>Topografia Corneale</h2>
                         <p>La Topografia Corneale, anche definita “mappa”, ci fornisce tramite l’ausilio di un computer una rappresentazione grafica della curvatura della cornea. Tramite la proiezione di una serie di anelli illuminati sulla superficie della cornea, che poi vengono acquisiti dallo strumento, il topografo genera una “mappa corneale”.</p>
-                        <button class="detail-button mb-3">Dettagli</button>
+                        <button class="detail-button mb-3" href='https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwjn47acutz4AhV9SfEDHdTKCmkQFnoECAkQAQ&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FTopografia_corneale&usg=AOvVaw1kaIKmap__kbi4UAYNiwv3'>Dettagli</button>
                     </div>
                 </div>
             </div>
@@ -69,7 +69,7 @@
                     <div>
                         <h2>Fluorangiografia</h2>
                         <p>Si esegue grazie all’utilizzo del Fluorangiografo, una particolare attrezzatura fotografica computerizzata che ci fornisce immagini in sequenza del fondo dell’occhio per circa 5 minuti, dopo aver dilatato la pupilla ed aver iniettato un colorante in una vena del braccio.</p>
-                        <button class="detail-button mb-3">Dettagli</button>
+                        <button class="detail-button mb-3" href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiU-ZmJutz4AhXDOHoKHW8SC38QFnoECA0QAQ&url=https%3A%2F%2Fit.wikipedia.org%2Fwiki%2FAngiografia_con_fluorescina&usg=AOvVaw2mP7Vkml3NlIinUvQakDN8">Dettagli</button>
                     </div>
                 </div>
                 <div class="col-12 col-xl-6 d-flex justify-content-center order-1 order-xl-2"> 
